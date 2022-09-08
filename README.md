184eaf5c733195057ec9e006c08b84fdd335dff8
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-09-08 10:58:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# com-vaman-hr-demo
+ HR management app for EYGDS trainees Aug 2022
---
+++ src/main/java/com/vaman/hr/demo/App.java
@@ -0,0 +1,15 @@
+package com.vaman.hr.demo;
+
+/**
+ * 
+ * @author Vaman Deshmukh
+ * 
+ *         HR management app for EYGDS trainees Aug 2022
+ *
+ */
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
